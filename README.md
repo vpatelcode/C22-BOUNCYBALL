d5d278f879d983c8e09b3424066305b104fcb33e
vpatelcode <72821561+vpatelcode@users.noreply.github.com>
2020-10-29 16:50:22 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C22-BOUNCYBALL
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,43 @@
+// Namespacing
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+const World = Matter.World;
+
+var engine, world;
+var ground;
+var ball;
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+ 
+  var ground_options =  {
+  isStatic : true
+  }
+  ground = Bodies.rectangle(200,390,400,20, ground_options);
+  World.add(world,ground);
+
+  var ball_options= {
+    restitution:1
+  }
+  ball = Bodies.circle(200,200,20,ball_options);
+  World.add(world,ball);
+
+console.log(ground);
+}
+
+function draw() {
+  background(0,0,255);
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, 400,20);
+  circle(ball.position.x,ball.position.y,20);
+
+  }
+
+
+
+
+
+
+  
